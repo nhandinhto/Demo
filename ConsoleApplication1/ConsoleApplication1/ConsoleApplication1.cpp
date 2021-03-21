b00a1b862e20883c43a7461b727d288eefbdf877
Tô Đình Nhân <81056565+nhandinhto@users.noreply.github.com>
2021-03-21 09:39:11 +0700

    firstcommit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("hello world\n");
+	printf("First Test Gifhub\n");
+	printf("Failed");
+	system("PAUSE");
+	return 0;
+}
